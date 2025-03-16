d11b04b12d98df9430b5008b90b9e652c2a3a586
Lars <larsw02@gmail.com>
2025-03-16 21:44:34 +0000

    Updated Robot Commander and main for testing

--- ROS-main/control/src/main.py
+++ ROS-main/control/src/main.py
@@ -2,10 +2,75 @@
 
 from robot_commander import RobotCommander
 import rospy
+from geometry_msgs.msg import Pose
+import tf.transformations as tft
+from typing import List
+
+
+def get_pre_grasp_pose(x, y, z, rotation) -> Pose:
+    pose = Pose()
+    quats = tft.quaternion_from_euler(0, 0, rotation)
+    pose = Pose()
+    pose.orientation.x = quats[0]
+    pose.orientation.y = quats[1]
+    pose.orientation.z = quats[2]
+    pose.orientation.w = quats[3]
+
+    pose.position.x = x
+    pose.position.y = y
+    pose.position.z = z
+
+    return pose
+
+def get_target_pose(x, y, z) -> Pose:
+
+    quats = tft.quaternion_from_euler(0, 0, 0)
+    pose = Pose()
+    pose.orientation.x = quats[0]
+    pose.orientation.y = quats[1]
+    pose.orientation.z = quats[2]
+    pose.orientation.w = quats[3]
+
+    pose.position.x = x
+    pose.position.y = y
+    pose.position.z = z
+
+    return pose
+
+
+def build_pre_grasp_poses(pre_grasp_positions: List[Pose]) -> List[Pose]:
+    pre_grasp_poses = []
+    for pre_grasp_position in pre_grasp_positions:
+        pre_grasp_pose = get_target_pose(*pre_grasp_position)
+        pre_grasp_poses.append(pre_grasp_pose)
+
+    return pre_grasp_poses
+
+
+def build_target_poses(target_positions: List[Pose]) -> List[Pose]:
+    target_poses = []
+    for target_position in target_positions:
+        pre_grasp_pose = get_pre_grasp_pose(*target_position)
+        target_poses.append(pre_grasp_pose)
+
+    return target_poses
 
 
 def main():
     robot_commander = RobotCommander()
+    # (x, y, z)
+    target_positions = [()]
+    # (x, y, z, orientation)
+    pre_grasp_positions = [()]
+
+    target_poses = build_target_poses(target_positions)
+    print(f"Target Poses: \n{target_poses}")
+    pre_grasp_poses = build_pre_grasp_poses(pre_grasp_positions)
+    print(f"Pre Grasp Poses: \n{pre_grasp_poses}")
+
+    robot_commander.test_update_pose(target_poses=target_poses)
+    robot_commander.run(pre_grasp_poses)
+
 
 
 if __name__ == '__main__':
--- ROS-main/control/src/robot_commander.py
+++ ROS-main/control/src/robot_commander.py
@@ -9,6 +9,7 @@ import tf.transformations as transformations
 from geometry_msgs.msg import Pose
 import numpy as np
 from robot_controller import RobotMover
+from typing import List
 
 
 class RobotCommander(object):
@@ -20,36 +21,53 @@ class RobotCommander(object):
 
         # self.voice_commander = ...
         # self.perception_controller = ...
-        self.pyramid_points = []
+        self.pyramid_points = None
         self.idx = 0
 
 
         rospy.loginfo("Robot Commander initialized")
 
 
-    def run(self, cube_pose: Pose) -> None:
+    def run(self, cube_poses: List[Pose]) -> None:
+        """
+        Runs the overall cube stacking algorithm
+        Assumes a list of poses given for the whole current layer
+        """
         is_ready_pose: bool = self.robot_mover.is_in_ready_pose()
+        if self.pyramid_points is None:
+            raise TypeError("Pyramid Points is None, call update method first")
 
-        if is_ready_pose:
-            pre_grasp_motion_success: bool = self.robot_mover.move_to_pregrasp(cube_pose=cube_pose)
-            if pre_grasp_motion_success:
-                rospy.sleep(1)
-                move_down_success: bool = self.robot_mover.move_down()
-                if move_down_success:
+        i = 0
+        while i < len(cube_poses):
+            cube_pose = cube_poses[i]
+            if is_ready_pose:
+                pre_grasp_motion_success: bool = self.robot_mover.move_to_pregrasp(cube_pose=cube_pose)
+                if pre_grasp_motion_success:
                     rospy.sleep(1)
-                    grasp_success: bool = self.robot_mover.close_gripper()
-                    if grasp_success:
+                    move_down_success: bool = self.robot_mover.move_down()
+                    if move_down_success:
                         rospy.sleep(1)
-                        move_up_success: bool = self.robot_mover.move_up()
-                        if move_up_success:
+                        grasp_success: bool = self.robot_mover.close_gripper()
+                        if grasp_success:
                             rospy.sleep(1)
-                            motion_sucess = self.robot_mover.move_to_pose(self.pyramid_points[self.idx])
-                            if motion_sucess:
-                                self.update_pyramid_pose()
+                            move_up_success: bool = self.robot_mover.move_up()
+                            if move_up_success:
+                                rospy.sleep(1)
+                                motion_sucess = self.robot_mover.move_to_pose(self.pyramid_points[self.idx])
+                                if motion_sucess:
+                                    i += 1
+                                    self.update_pyramid_pose()
         
-        self.robot_mover.move_to_ready_pose()
-
+            self.robot_mover.open_gripper()
+            rospy.sleep(1)
+            is_ready_pose = self.robot_mover.move_to_ready_pose()
 
     
     def update_pyramid_pose(self) -> None:
         rospy.logerr("update_pyramid_pose currently not mplemented")
+        rospy.sleep(1)
+
+
+    def test_update_pose(self, target_poses: List[Pose]) -> None:
+        self.pyramid_points = target_poses
+        rospy.sleep(1)
